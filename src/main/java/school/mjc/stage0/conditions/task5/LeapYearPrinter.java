a23736425971cb14b02d2ecd16143f4bc28ad380
THETOUCH <super.ilgiznex@gmail.com>
2023-10-19 19:06:59 +0600

    123

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int x;
+        x = year % 400 == 0 || year % 100 != 0 && year % 4 == 0 ? 1 : 0;
+        switch (x) {
+            case 1:
+                System.out.println("leap");
+                break;
+            case 0:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
